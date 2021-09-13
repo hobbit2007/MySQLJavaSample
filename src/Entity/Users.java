d3c54c1bb55720cc02dcd7281a711b4f074cf854
r.grytsay <roman-grytsay@yandex.ru>
2021-09-13 11:23:33 +0300

    Класс модель описывающий структуру таблицы users

---
+++ src/Entity/Users.java
@@ -0,0 +1,47 @@
+package Entity;
+
+/**
+ * Класс модель описывающий структуру таблицы users
+ */
+
+public class Users {
+
+    private Long id;
+    private String login;
+    private String passwd;
+    private String role;
+
+    public Users() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
