cb751dff7849ac203cda67db81e0ec870e594a0b
r.grytsay <roman-grytsay@yandex.ru>
2021-09-13 12:38:13 +0300

    Изменил вызов диалогового окна

--- src/db/ConnectToDB.java
+++ src/db/ConnectToDB.java
@@ -1,18 +1,16 @@
 package db;
 
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-
+import dialog.DialogAlert;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Optional;
 
 public class ConnectToDB {
     private static final String url = "jdbc:mysql://192.168.100.245:3306/hmmr_mu";
     private static final String user = "логин к БД";
     private static final String password = "пароль к БД";
     public static Connection con;
+    DialogAlert dialogAlert = new DialogAlert();
 
     public ConnectToDB() {
 
@@ -26,7 +24,7 @@ public class ConnectToDB {
             // opening database connection to MySQL server
             con = DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
-            AlertDialog(e.getMessage(), "Внимание! Ошибка подключения к БД.");
+            dialogAlert.createDialog("Ошибка!", "Внимание! Ошибка подключения к БД.", true).setVisible(true);
         }
     }
 
@@ -36,31 +34,8 @@ public class ConnectToDB {
             // opening database connection to MySQL server
             con = DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
-            AlertDialog(e.getMessage(), "Внимание! Ошибка подключения к БД.");
+            dialogAlert.createDialog("Ошибка!", "Внимание! Ошибка подключения к БД.", true).setVisible(true);
         }
         return con;
     }
-
-    /**
-     * Вызов диалогового окна без надписи в титле
-     * @param str - Сообщение которое выведет диалоговое окно
-     * @param str1 - Сообщение которое будет в титле
-     */
-    public static void AlertDialog(String str, String str1)
-    {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle(str1);
-
-        alert.setHeaderText(str);
-
-        Optional<ButtonType> option = alert.showAndWait();
-        if (option.get() == null) {
-
-        } else if (option.get() == ButtonType.OK) {
-
-        } else if (option.get() == ButtonType.CANCEL) {
-
-        }
-    }
-
 }
