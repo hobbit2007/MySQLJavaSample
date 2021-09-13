feaaea1ca6c963596c474481e39a8454bb967218
r.grytsay <roman-grytsay@yandex.ru>
2021-09-13 12:39:43 +0300

    Основной класс приложения

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import Entity.Users;
+import db.ConnectToDB;
 import db.SqlQuery;
+import dialog.DialogAlert;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -13,25 +15,33 @@ public class Main{
 
     public static void main(String args[]) {
         SqlQuery sqlQuery = new SqlQuery();
+        DialogAlert dialogAlert = new DialogAlert();
 
         JFrame a = new JFrame("example");
-        JTextField b = new JTextField("Введите логин:");
+        JTextField b = new JTextField();
+        b.setToolTipText("Введите логин:");
         JButton button = new JButton("Нажми меня");
 
         //Обработчик нажатия на кнопку
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //Получили из БД данные по пользователю
-                List<Users> userList = sqlQuery.checkLogin(b.getText());
-                //Вставили в текстовое поле роль пользователя
-                b.setText(userList.get(0).getRole());
+                if (!b.getText().isEmpty()) {
+                    //Получили из БД данные по пользователю
+                    List<Users> userList = sqlQuery.checkLogin(b.getText());
+                    //Вставили в текстовое поле роль пользователя
+                    b.setText(userList.get(0).getRole());
+                }
+                else
+                   dialogAlert.createDialog("Ошибка!", "Внимание! Введите логин.", true).setVisible(true);
             }
         });
 
         b.setBounds(50,100,200,30);
+        button.setBounds(300,100,150,30);
         a.add(b);
-        a.setSize(300,300);
+        a.add(button);
+        a.setSize(500,300);
         a.setLayout(null);
         a.setVisible(true);
     }
